8826ac8776224124d25980a126fe8cad4be6d842
oowtl <oowtl7425@gmail.com>
2025-04-14 14:10:31 +0900

    조합 - BOJ_1759 - 문자열로 추가하기

--- 기본 알고리즘/순열과 조합/BOJ 1759/boj_1759.py
+++ 기본 알고리즘/순열과 조합/BOJ 1759/boj_1759.py
@@ -5,7 +5,6 @@ sys.stdin = open(os.path.join(os.path.dirname(__file__), 'input.txt'), encoding=
 """
 === input.txt 를 불러오기 위한 코드 ===
 """
-
 # 암호 글자 길이, 주어진 글자 길이
 L, C = map(int, input().split())
 arr = input().split()
@@ -15,32 +14,27 @@ arr.sort()
 
 max_level = L
 s_length = C
-c_arr = ["a", "e", "i", "o", "u"]
+vows = ["a", "e", "i", "o", "u"]
 
-# 완료 조건 체크
+
+# 모음 체크
 def check(sentence: str):
-    # 모음 개수
-    m_cnt = 0
-    # 자음 개수
-    j_cnt = 0
+    global vows, s_length
 
+    vow = 0
     for s in sentence:
-        if s in c_arr:
-            m_cnt += 1
-        else:
-            j_cnt += 1
+        if s in vows:
+            vow += 1
+    con = max_level - vow
+    return vow >= 1 and con >= 2
 
-    if m_cnt >= 1 and j_cnt >= 2:
-        return True
-    else:
-        return False
 
 def combination(index, acc):
-    if max_level <= len(acc) and check(acc):
+    if max_level == len(acc) and check(acc):
         print(acc)
-        return
 
     for i in range(index, s_length):
         combination(i + 1, acc + arr[i])
+
 
 combination(0, "")
