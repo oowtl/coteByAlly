1cd23488c58d3f4d9a35d9c690d3e071dafe3f3e
oowtl <oowtl7425@gmail.com>
2025-05-17 15:07:47 +0900

    DP 알고리즘 - 1149

--- 필수 알고리즘/DP/BOJ 11048/boj_11048.py
+++ 필수 알고리즘/DP/BOJ 11048/boj_11048.py
@@ -44,13 +44,34 @@ f(r, c) = max(f(r-1, c), f(r, c-1), f(r-1, c-1)) + arr[r][c]
 
 
 """
-N, M = map(int, input().split())
+import sys
+sys.setrecursionlimit(int(1e6))
 
-arr = [list(map(int, input().split())) for _ in range(N)]
+INF = int(1e12)
 
-print(arr)
+def func(n, k): # dp[n][k]를 반환
+	global cost, dp
 
-max_arr = [[0] * M] * N
-result = 0
+	# base case
+	if dp[n][k] != -1:
+		return dp[n][k]
+
+	# recursive case
+	best = INF
+	for i in range(3):
+		if i != k:
+			best = min(best, func(n - 1, i))
+
+	dp[n][k] = best + cost[n][k]
+	return dp[n][k]
+
+
+N = int(input())
+cost = [[0, 0, 0]]
+cost += [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0, 0, 0]] + [[-1, -1, -1] for _ in range(N)]
+
+print(min(func(N, 0), func(N, 1), func(N, 2)))
 
 
